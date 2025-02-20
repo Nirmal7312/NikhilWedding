a583296c78ff46b074762d8155bcd621828652fd
Nirmal <aman.kumar@ncfe.ac.in>
2025-02-20 22:36:56 +0530

    Initial Commit

---
+++ NikhilWedding/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import os
+from django.conf import settings  # Correct way to import settings
+def index(request):
+    ti = 364
+    images = [f'img1 ({i}).jpg' for i in range(1, ti + 1)]
+    for img in images:
+        print(img)
+    params = {'images':images}
+    return render(request, 'index.html', params)
+
+def view_img(request, img_name):
+    params = {'img_name':img_name}
+    return render(request, 'view.html', params)
